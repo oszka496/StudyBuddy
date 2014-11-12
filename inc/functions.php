61d3b733e16d886d22f5a40ce2e468e990615639
TeoTN <staniowp@gmail.com>
2014-11-12 23:26:44 +0100

    small fixes

--- inc/functions.php
+++ inc/functions.php
@@ -1,6 +1,6 @@
 <?php
 	header('Content-type: text/html; charset=utf-8');
-	global mysqli;
+	global $mysqli;
 	$mysqli = mysqli_connect('db4free.net', 'adminer', 'axis!69', 'studybuddy') or die(mysqli_error($mysqli));
 	if(!isset($_SESSION['id'])) {
 	 	session_start();
@@ -19,7 +19,7 @@
 		if(isset($_SESSION['id']) and isset($_SESSION['firstName']) and isset($_SESSION['lastName'])){
 			$tab[0] = $_SESSION['id'];
 			$tab[1] = $_SESSION['firstName'];
-			$tab[2] = $_SESSION['lastName']
+			$tab[2] = $_SESSION['lastName'];
 			return $tab;
 		} else {
 			return "None";
--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-	require_once 'functions.php';
+	require_once 'inc/functions.php';
 	
 	if (isset($_POST['username']) && isset($_POST['password'])){
 		

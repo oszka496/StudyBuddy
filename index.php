ab24541ae61435f45d10a8230c8e5f7724ad8e61
TeoTN <staniowp@gmail.com>
2014-11-12 22:09:33 +0100

    Initial scheme

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+ 	session_start();
+	include 'inc/functions.php';
+	global $mysqli;
+?>
+<!DOCUMENT HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>StudyBuddy</title>
+	<script type="text/javascript" src="inc/jQuery.js"></script>
+	<link type="text/css" rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header>
+		<h1></h1>
+		<form action="login.php" method="post" id="login-form">
+		</form>
+	</header>
+	<section>
+		<aside></aside>
+		<article></article>
+	</section>
+</body>
+</html>

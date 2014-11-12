3701bcc2d904817edf901235aa7686df1163bfd9
TeoTN <staniowp@gmail.com>
2014-11-12 22:26:08 +0100

    Revert "Manager sketch"
    This reverts commit 4e6b362bfbfe3f6c2bd38d6567227c56912d3040.

---
+++ inc/manager.js
@@ -0,0 +1,23 @@
+// "self-invoking" function
+(function manger() {
+    // Load the script
+    var script = document.createElement("script");
+    script.src = 'http://localhost/inc/jquery-1.11.1.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName("head")[0].appendChild(script);
+
+    // Poll for jQuery to come into existance
+    var checkReady = function(callback) {
+        if (window.jQuery) {
+            callback(jQuery);
+        }
+        else {
+            window.setTimeout(function() { checkReady(callback); }, 100);
+        }
+    };
+
+    // Start polling...
+    checkReady(function($) {
+        alert("jQuery ready");
+    });
+})();

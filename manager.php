ccc9f17d881921e89e1cd7ce34435930a2ebe81b
TeoTN <staniowp@gmail.com>
2014-11-12 23:23:22 +0100

    Manager improvement
    Check if user is logged in

--- manager.php
+++ manager.php
@@ -10,6 +10,21 @@
 <body>
 	<div id='study-buddy-manager'>
 		<h1>StudyBuddy</h1>
+		<?php
+			$user = getUser();
+			$id = $name = $surname = "";
+			if ($user == "None")
+			{
+				echo "<p>You're not logged in.</p>";
+				echo "<a href='index.php'>Log in</a>";
+			}
+			else
+			{
+				$id = $user[0];
+				$name = $user[1];
+				$surname = $user[2];
+			}
+		?>
 	</div>
 </body>
 </html>

af5d99c516ba912f41768b431332bae8e25f9144
TeoTN <staniowp@gmail.com>
2014-11-18 11:32:42 +0100

    Link parsing - proposed fix

--- inc/manager.js
+++ inc/manager.js
@@ -25,8 +25,8 @@
             if (h!="#" && typeof h !== "undefined" && h != "") {
                 if (!h.match("^http")) {
                     console.log(url);
-                    if (h.charAt(h.length - 1) != "/")
-                        h = h.substring(0, h.lastIndexOf("/"));
+                    if (h.charAt(h.length-1) != "/")
+                        h = h.substring(0, h.lastIndexOf("/")+1);
                     h = url + h;
                 }
                 hrefs.push(h);

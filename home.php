ca7f29c56b855601d869417542c17300114fbb81
oszka496 <oszka496@gmail.com>
2014-11-29 23:32:26 +0100

    Home edit

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
 <ul class="nav nav-tabs" role="tablist" id="myTab">
   <li role="presentation" class="active"><a href="#observed" aria-controls="observed" role="tab" data-toggle="tab">Observed courses</a></li>
   <li role="presentation"><a href="#available" aria-controls="available" role="tab" data-toggle="tab">Available courses</a></li>
-  <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
+  <li role="presentation"><a href="#universities" aria-controls="universities" role="tab" data-toggle="tab">Universities</a></li>
   <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab"></a></li>
 </ul>
 
@@ -17,8 +17,8 @@
   <div role="tabpanel" class="tab-pane" id="available">
     Your university: <strong>My UNI</strong>
   </div>
-  <div role="tabpanel" class="tab-pane" id="messages">
-    
+  <div role="tabpanel" class="tab-pane" id="universities">
+    <?php include("university.php")?>
   </div>
   <div role="tabpanel" class="tab-pane" id="settings">stg...</div>
 </div>

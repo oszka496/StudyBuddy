150575706dc3e0fdf5c27d0fa5ffbc833a2d57a0
oszka496 <oszka496@gmail.com>
2014-12-04 22:24:30 +0100

    Bug fixed in User

--- api/user.php
+++ api/user.php
@@ -133,10 +133,10 @@ class User
 		if(!isSessionSet()) 
 			throw new Exception("Session wasn't set.");
 		$id = s($_SESSION['id']);
-		if((s($SESSION_['uType']) != 0) && $id != getUserIdByMail($mail)[0]) return $INSUFFICIENT_PRIVILEGE;
+		if((s($SESSION_['uType']) != 0) && $id != getUserIdByMail($mail)[0]) return User::$INSUFFICIENT_PRIVILEGE;
 		$query = "CALL delete_user('$mail');";
 		$result = mysqli_query($mysqli, $query) or die(mysqli_error($mysqli));
-		return $DELETE_SUCCESS;
+		return User::$DELETE_SUCCESS;
 	}
 }
 

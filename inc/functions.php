a653814c6a3ff2daa794ddf5f58c4a2932d91b11
oszka496 <oszka496@gmail.com>
2014-12-04 23:28:23 +0100

    switch to openshift db

--- inc/functions.php
+++ inc/functions.php
@@ -1,6 +1,6 @@
 <?php
 	header('Content-type: text/html; charset=utf-8');
-	require_once 'db_db4free.cfg.php';
+	require_once 'db_openshift.cfg.php';
 	require_once dirname(__FILE__).'\..\api\user.php';
 	require_once dirname(__FILE__).'\..\api\course.php';
 	require_once dirname(__FILE__).'\..\api\problemSet.php';

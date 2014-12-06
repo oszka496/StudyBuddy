89f055e1bb4714b1cb7d24280bdfd2ceeb8fea9b
TeoTN <staniowp@gmail.com>
2014-12-06 15:37:09 +0100

    API request course, uni

--- api/request.php
+++ api/request.php
@@ -53,6 +53,141 @@
 			}
 			break;
 
+		case 'addcourse':
+			$credentials = $requestData["credentials"];
+			auth($credentials['login'], $credentials['hash']);
+			try
+			{
+				$result = Course::addCourse($requestContent["name"], $requestContent["address"],
+					$requestContent["universityid"], $requestContent["startdate"], $requestContent["enddate"]);
+				respond($result, "");
+			}
+			catch (Exception $e)
+			{
+				respond("-1", "EXCEPTION: ".$e->getMessage());
+			}
+			break;
+
+		case 'deletecourse':
+			$credentials = $requestData["credentials"];
+			auth($credentials['login'], $credentials['hash']);
+			try
+			{
+				$result = Course::deleteCourse($requestContent["courseid"]);
+				respond($result, "");
+			}
+			catch (Exception $e)
+			{
+				respond("-1", "EXCEPTION: ".$e->getMessage());
+			}
+			break;
+
+
+		case 'updatefield':
+			$credentials = $requestData["credentials"];
+			auth($credentials['login'], $credentials['hash']);
+			try
+			{
+				$result = Course::updateField($requestContent["field"], $requestContent["courseid"],
+					$requestContent["value"]);
+				respond($result, "");
+			}
+			catch (Exception $e)
+			{
+				respond("-1", "EXCEPTION: ".$e->getMessage());
+			}
+			break;
+
+		case 'editcourse':
+			$credentials = $requestData["credentials"];
+			auth($credentials['login'], $credentials['hash']);
+			try
+			{
+				$result = Course::editCourse($requestContent["courseid"], $requestContent["lecturerid"],
+					$requestContent["coursename"], $requestContent["coursestart"], $requestContent["courseend"],
+					$requestContent["courseaddress"], $requestContent["universityid"]);
+				respond($result, "");
+			}
+			catch (Exception $e)
+			{
+				respond("-1", "EXCEPTION: ".$e->getMessage());
+			}
+			break;
+
+		case 'getaddressbyid':
+			$credentials = $requestData["credentials"];
+			auth($credentials['login'], $credentials['hash']);
+			try
+			{
+				$result = Course::getAdressById($requestContent["courseid"]);
+				respond($result, "");
+			}
+			catch (Exception $e)
+			{
+				respond("-1", "EXCEPTION: ".$e->getMessage());
+			}
+			break;
+
+		case 'enrolltocourse':
+			$credentials = $requestData["credentials"];
+			auth($credentials['login'], $credentials['hash']);
+			try
+			{
+				$result = Course::enrollToCourse($requestContent["courseaddress"]);
+				respond($result, "");
+			}
+			catch (Exception $e)
+			{
+				respond("-1", "EXCEPTION: ".$e->getMessage());
+			}
+			break;
+
+		case 'createuniversity':
+			$credentials = $requestData["credentials"];
+			auth($credentials['login'], $credentials['hash']);
+			try
+			{
+				$result = Course::createUniversity($requestContent["universityname"],
+					$requestContent["universityaddress"], $requestContent["tags"],
+					$requestContent["universitymail"]);
+				respond($result, "");
+			}
+			catch (Exception $e)
+			{
+				respond("-1", "EXCEPTION: ".$e->getMessage());
+			}
+			break;
+
+		case 'deleteuniversity':
+			$credentials = $requestData["credentials"];
+			auth($credentials['login'], $credentials['hash']);
+			try
+			{
+				$result = Course::deleteUniversity($requestContent["universityid"]);
+				respond($result, "");
+			}
+			catch (Exception $e)
+			{
+				respond("-1", "EXCEPTION: ".$e->getMessage());
+			}
+			break;
+
+		case 'edituniversity':
+			$credentials = $requestData["credentials"];
+			auth($credentials['login'], $credentials['hash']);
+			try
+			{
+				$result = Course::editUniversity($requestContent["universityid"].
+					$requestContent["universityname"], $requestContent["universityaddress"],
+					$requestContent["tags"], $requestContent["universitymail"]);
+				respond($result, "");
+			}
+			catch (Exception $e)
+			{
+				respond("-1", "EXCEPTION: ".$e->getMessage());
+			}
+			break;
+
 		default:
 			# code...
 			break;
